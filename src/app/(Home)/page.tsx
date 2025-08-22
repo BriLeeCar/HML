9e2f69d222a3c67cca326e3365c068e78a970fa9
BriLeeCar <118677926+BriLeeCar@users.noreply.github.com>
2025-08-22 00:25:40 -0500

    Updated home page

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,4 +1,4 @@
-import { tCountryKeys, tCountryPathData } from '@/(Content)/map/util'
+import { tCountryKeys } from '@/(Content)/map/util'
 import countryPaths from '~/data/countryDataWithPaths.json'
 import { MapPathEl, MapSvg } from '../(Content)/map/Map'
 import { MapBtn, NowBtn, SmallBtns } from './Buttons'
@@ -19,21 +19,22 @@ const Home = async () => {
 			</div>
 			<div className='absolute -top-10 -right-40 -z-1 hidden h-1/2 w-[50vw] -rotate-5 overflow-clip min-[600px]:h-screen min-[1200px]:translate-y-0 min-[1200px]:rotate-0 md:block'>
 				<MapSvg className='lg:max-h-fill xl: text-primary-foreground h-auto w-screen shadow-none lg:relative lg:w-screen'>
-					{Object.keys(countryPaths).map((countryName) => {
-						const { path, tier, haveData, abbr } = countryPaths[
-							countryName as tCountryKeys
-						] as tCountryPathData
-						return (
-							<MapPathEl
-								key={countryName}
-								name={countryName as tCountryKeys}
-								abbr={abbr}
-								tier={tier}
-								haveData={haveData}
-								path={path}
-							/>
-						)
-					})}
+					{countryPaths
+						.filter((ea) => ea.path)
+						.map((country) => {
+							const { path, tier, haveData, abbr, name } = country
+
+							return (
+								<MapPathEl
+									key={name}
+									name={name as tCountryKeys}
+									abbr={abbr}
+									tier={tier || 999}
+									haveData={haveData || false}
+									path={path || ''}
+								/>
+							)
+						})}
 				</MapSvg>
 			</div>
 		</>
