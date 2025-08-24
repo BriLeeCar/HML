1ed0b0b132e34812eb7ad6a5403b338e389c5fc8
BriLeeCar <118677926+BriLeeCar@users.noreply.github.com>
2025-08-24 02:31:54 -0500

    Cont.

---
+++ src/app/(Content)/(Full)/explorer/Filter.tsx
@@ -0,0 +1,20 @@
+import { Icon } from '~/components/Icon'
+import { Button } from '~/components/ui'
+
+export const Filter = ({
+	count,
+	...props
+}: Props<typeof Button> & { count: number }) => {
+	return (
+		<span className='*:click absolute right-20 flex gap-2'>
+			<Button
+				{...props}
+				variant={'default'}
+				size={'sm'}
+				className='bg-muted text-muted-foreground text-sm font-semibold uppercase'>
+				<Icon IconName='FilterIcon' />
+				Filters ({count})
+			</Button>
+		</span>
+	)
+}
