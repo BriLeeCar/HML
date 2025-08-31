24eb1c05648983ef095690c20fe9fa862ddd2837
BriLeeCar <118677926+BriLeeCar@users.noreply.github.com>
2025-08-31 16:24:30 -0500

    Clear branch dev

---
+++ src/app/(Content)/(Constrained)/guides-resources/EvidenceTimeline.tsx
@@ -0,0 +1,10 @@
+import { SectionHeading } from '~/components/Heading'
+import { Section } from '~/components/Page'
+
+export const EvidenceTimeline = () => {
+	return (
+		<Section>
+			<SectionHeading>Evidence Timeline</SectionHeading>
+		</Section>
+	)
+}
