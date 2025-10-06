195d6d4aa77cb7ee7ea6109a39dcacfa3daab437
BriLeeCar <118677926+BriLeeCar@users.noreply.github.com>
2025-10-06 08:04:42 -0500

    Troubleshooting middleware URL error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,13 @@
-import { auth } from "~/server/auth";
+import { auth } from '~/server/auth'
 
 export default auth((req) => {
-	if (!req.auth && req.nextUrl.pathname !== "/login") {
-		const newUrl = new URL("/api/auth/signin");
-		return Response.redirect(newUrl);
+	console.log(req)
+	if (!req.auth && req.nextUrl.pathname !== '/login') {
+		const newUrl = new URL(`/api/auth/signin`, req.nextUrl.origin)
+		return Response.redirect(newUrl)
 	}
-});
+})
 
 export const config = {
-	matcher: ["/admin((?:/[^auth]).*)?"],
-};
+	matcher: ['/admin((?:/[^auth]).*)?'],
+}
