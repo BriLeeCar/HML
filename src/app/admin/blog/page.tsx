cf9e9214014935d82298c40b4bcd2e0b5afc0036
BriLeeCar <118677926+BriLeeCar@users.noreply.github.com>
2025-10-06 19:18:15 -0500

    fdsa

--- src/app/admin/blog/page.tsx
+++ src/app/admin/blog/page.tsx
@@ -109,8 +109,8 @@ const BlogPage = async () => {
 							<TableCell>{toShortDate(post.createdAt)}</TableCell>
 							<TableCell>
 								<div className='flex flex-wrap gap-1'>
-									{post.tags
-										&& post.tags?.map(({ tag }, index) => (
+									{Array.isArray(post.tags)
+										&& post.tags.map(({ tag }, index) => (
 											<span
 												key={index}
 												className='rounded-full bg-zinc-200 px-2 py-1 text-xs text-zinc-800 dark:bg-zinc-700 dark:text-zinc-200'>
