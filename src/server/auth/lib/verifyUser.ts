436246bd3f251cac57206fed1844f67ed636d63c
BriLeeCar <118677926+BriLeeCar@users.noreply.github.com>
2026-01-19 08:34:41 -0600

    User verification failed. Redirecting to sign-in page.

--- src/server/auth/lib/verifyUser.ts
+++ src/server/auth/lib/verifyUser.ts
@@ -1,10 +1,9 @@
 import { redirect } from 'next/navigation'
 import { auth } from '..'
-import { api } from '../../../trpc/server'
 
 export const verifyUser = async () => {
-	const { id } = (await auth())?.user ?? {}
-	const user = await api.user.getUserById(id)
-	if (!user) redirect('/admin/auth/signin')
-	return user
+	const authed = await auth()
+	const user = authed?.user
+	console.log(user)
+	!user && redirect('/admin/auth/signin')
 }
