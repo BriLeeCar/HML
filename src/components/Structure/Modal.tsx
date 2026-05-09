7ca9f8c63b0114f6de8f6b7c9ac323711bdfa952
Eden Fenn <edenfenn@Summoning-Slate.local>
2026-05-09 12:33:23 -0400

    Adds a CloseButton to Button.tsx

--- src/components/Structure/Modal.tsx
+++ src/components/Structure/Modal.tsx
@@ -7,7 +7,7 @@ import { Eyebrow } from '~/components/Text/Eyebrow'
 import { Heading } from '~/components/Text/Heading'
 import { Subtitle } from '~/components/Text/Subtitle'
 import { cn } from '~/lib/cn'
-import { Button } from '../Button';
+import { Button, CloseButton } from '../Button';
 
 export const Modal = ({ ...props }: Props) => {
     const [open, setOpen] = useState(false);
@@ -18,10 +18,10 @@ export const Modal = ({ ...props }: Props) => {
             >Open me</Button>
             <MuiModal open={open} className="w-full h-full bg-background/50 m-0 p-0 flex justify-center items-center">
                 <div className="modal__body bg-background flex flex-col gap-6 size-9/10 flex-shrink-0 rounded-xl pt-0 pb-10 px-6">
-                    <div className="modal__header">
-                        <Heading>Heading</Heading>
-                        <Subtitle>Subtitle</Subtitle>
-                    </div>
+                    <div className="modal__header flex w-full justify-between">
+                        <Heading className="h2">Heading</Heading>
+                        <CloseButton />
+                    </div>/
 
                         {props.children}
                 </div>
