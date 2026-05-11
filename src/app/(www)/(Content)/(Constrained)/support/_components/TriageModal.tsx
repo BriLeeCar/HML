531a3b3e14e061a146034bdef13ab4a93a1947b9
Eden Fuller <eden.fuller@pm.me>
2026-05-11 01:38:12 -0400

    Makes TriageModal render choice buttons and redirect

--- src/app/(www)/(Content)/(Constrained)/support/_components/TriageModal.tsx
+++ src/app/(www)/(Content)/(Constrained)/support/_components/TriageModal.tsx
@@ -1,10 +1,40 @@
+'use client'
+
+import { useState } from 'react';
+import Markdown from 'react-markdown'
+import { redirect, RedirectType } from 'next/navigation'
+
 import { Modal } from "~/components/Structure/Modal";
+import { Button } from '~/components/Button';
+import { triagePages, type Branch } from '../_lib/Triage';
+import './TriageStyles.css'
 
 export function TriageModal() {
+    const [branch, setBranch] = useState('intro');
+
+    const branchContent: Branch = triagePages[branch];
+
+    if(branchContent.redirect) {
+        redirect(branchContent.redirect, 'push' as RedirectType)
+    }
+
     return (<Modal
         id="onsite-triage"
         btnText="triage"
     >
-        <div>xoxo</div>
+        {branchContent.body && (
+            <Markdown>{branchContent.body}</Markdown>
+        )}
+        {branchContent.choices && (
+            <div className="flex gap-3">
+                {branchContent.choices.map((choice) => (
+                    <Button
+                        onClick={() => setBranch(choice.next)}
+                    >
+                        {choice.text}
+                    </Button>
+                ))}
+            </div>
+        )}
     </Modal>)
 }
