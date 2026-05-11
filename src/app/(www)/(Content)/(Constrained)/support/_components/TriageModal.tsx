11dde558ea77ac54d4c223ed1114382b1b009587
Eden Fuller <eden.fuller@pm.me>
2026-05-11 02:33:20 -0400

    Inputs triage content

--- src/app/(www)/(Content)/(Constrained)/support/_components/TriageModal.tsx
+++ src/app/(www)/(Content)/(Constrained)/support/_components/TriageModal.tsx
@@ -7,7 +7,7 @@ import { redirect, RedirectType } from 'next/navigation'
 import { Modal } from "~/components/Structure/Modal";
 import { Button } from '~/components/Button';
 import { triagePages, type Branch } from '../_lib/Triage';
-import './TriageStyles.css'
+import './TriageModal.css'
 
 export function TriageModal() {
     const [branch, setBranch] = useState('intro');
