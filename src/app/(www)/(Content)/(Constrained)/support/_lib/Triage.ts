3b3e0279912eaacfdc08fd383c97247f56ac8fa0
Eden Fuller <eden.fuller@pm.me>
2026-05-11 13:21:09 -0400

    Updates text content

--- src/app/(www)/(Content)/(Constrained)/support/_lib/Triage.ts
+++ src/app/(www)/(Content)/(Constrained)/support/_lib/Triage.ts
@@ -16,11 +16,11 @@ interface Triage {
 export const triagePages = <Triage> {
     intro: {
         body: `
-Help Me Leave helps people understand publicly-available information about migration and visas. We are not a licensed immigration advisory service, and nothing we provide is legal advice. 
+*Help Me Leave* helps people understand publicly-available information about migration and visas. We are not a licensed immigration advisory service, and nothing we provide is legal advice. 
 
 You must make your own decision based on your circumstances, your preferences and the compromises you are willing to make. We do not advise, direct or encourage anyone to leave their home country. You choose whether to stay or to leave, which country to apply to, and when to act.
 
-The Help Me Leave Support team is run by volunteers and has limited capacity. We focus on supporting the people who are at the **highest risk of harm right now** by helping them understand their options. This includes people who are: 
+The *Help Me Leave* Support team is run by volunteers and has limited capacity. We focus on supporting the people who are at the **highest risk of harm right now** by helping them understand their options. This includes people who are: 
 - Latino/Hispanic communities and Indigenous/Native Americans
 - Brown, Black and undocumented immigrants 
 - Black Americans
@@ -31,11 +31,11 @@ Please remember that we provide information and support, but we cannot guarantee
 
 If you are not in one of these groups, your Support ticket response time will likely be delayed. You are still welcome to:
 
-- Use the Help Me Leave website to find pathways [HelpmeleaveHML | Pathway Explorer](https://www.helpmeleave.us/explorer) 
-- Use Help Me Leave resources and guides [HelpmeleaveHML | Guides & Resources](https://www.helpmeleave.us/guides-resources)
-- Join our community Discord to support your research. [Join the Help Me Leave - Community Discord Server!](https://discord.gg/yt68AYNdt8) 
+- Use the *Help Me Leave* website to find pathways [HelpmeleaveHML | Pathway Explorer](https://www.helpmeleave.us/explorer) 
+- Use *Help Me Leave* resources and guides [HelpmeleaveHML | Guides & Resources](https://www.helpmeleave.us/guides-resources)
+- Join our community Discord to support your research. [Join the *Help Me Leave* - Community Discord Server!](https://discord.gg/yt68AYNdt8) 
         
-Do you understand how Help Me Leave can help you?
+Do you understand how *Help Me Leave* can help you?
         `,
         choices: [
             {
@@ -58,17 +58,17 @@ Do you need direct support with funding for your move?
         choices: [
             {
                 text: 'Yes',
-                next: 'funding'
+                next: 'exit-funding'
             },
             {
                 text: 'No',
-                next: 'exit-funding'
+                next: 'distress'
             },
         ]
     },
     'exit-funding': {
         body: `
-Help Me Leave does not provide direct funding of any kind. Our mission is to provide our community with information, resources and advocacy. We recommend you join our Discord community for many useful resources, tips, and personal experiences navigating the financial strain of relocation. [Join the Help Me Leave - Community Discord Server!](https://discord.gg/yt68AYNdt8)
+*Help Me Leave* does not provide direct funding of any kind. Our mission is to provide our community with information, resources and advocacy. We recommend you join our Discord community for many useful resources, tips, and personal experiences navigating the financial strain of relocation. [Join the *Help Me Leave* - Community Discord Server!](https://discord.gg/yt68AYNdt8)
         `,
         choices: [
             {
@@ -94,7 +94,7 @@ Are you experiencing significant emotional distress or a mental health crisis ri
     },
     'exit-distress': {
         body: `
-We understand that many people who contact Help Me Leave are under great stress. Our volunteers are not mental health professionals. 
+We understand that many people who contact *Help Me Leave* are under great stress. Our volunteers are not mental health professionals. 
 What we can do:
 - Listen with care
 - Offer calm, supportive direction to resources and information
@@ -141,7 +141,7 @@ Are you currently in a very unsafe environment at home, for example, because of 
     },
     'exit-emergency': { 
         body: `
-Help Me Leave offers non-emergency support only. If you are in immediate danger or need to leave your environment right now, please contact local emergency services or local support organizations immediately to ensure your safety.
+*Help Me Leave* offers non-emergency support only. If you are in immediate danger or need to leave your environment right now, please contact local emergency services or local support organizations immediately to ensure your safety.
 
 - [The National Coalition Against Domestic Violence](https://ncadv.org/)
 - [Need Homeless Assistance?](https://www.hudexchange.info/housing-and-homeless-assistance/homeless-help/)
@@ -156,15 +156,15 @@ Help Me Leave offers non-emergency support only. If you are in immediate danger 
     },
     form: {
         body: `
-The role of Help Me Leave is to help you understand your options. Your options will be constrained by:
+The role of *Help Me Leave* is to help you understand your options. Your options will be constrained by:
 - Your financial circumstances
 - Your job
 - Whether you have family members or pets
 - Immigration laws 
 
-You may come to the conclusion that your options are too limited or you do not like the options that you have. You may decide that you want to remain in your country of origin. **That is also a valid choice.** We have collected resources and useful information for those choosing to stay. (Is WeRemain live?)
+You may come to the conclusion that your options are too limited or you do not like the options that you have. You may decide that you want to remain in your country of origin. **That is also a valid choice.** We have collected resources and useful information for those choosing to stay.
 
-**Contacting the Help Me Leave Support team**
+**Contacting the *Help Me Leave* Support team**
 If you would like to ask the HML Support Team for **information, guidance, or resources** about leaving your country, please fill in [our contact form](https://forms.clickup.com/90151711045/f/2kyqbwa5-195/251K9UAW6C3E3HIJIG)`
     }
 }
